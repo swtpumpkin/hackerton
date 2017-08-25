df4269fd786642071acde4930cda6b7e87491f4f
downmix <downmix@downmix.net>
2017-08-25 11:09:33 +0900

    server edit

--- server.js
+++ server.js
@@ -3,42 +3,37 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
-// Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
+// json-server 페이트서버 커스텀
+// google : 트랜드 결과 가져오기
+
+server.get('/google', (req, res) => {
   //res.jsonp(req.query)
-  console.log(req.query.val1);
-  const val1 = req.query.val1;
-  const val2 = req.query.val2;
+  //console.log(res.jsonp(req.query));
+  const val1 = '갤럭시노트8';
+  const val2 = '아이폰7플러스';
 
   const googleTrends = require('google-trends-api');
   
   googleTrends.interestOverTime({keyword: [val1, val2], startTime: new Date('2017-08-01'), geo: "KR" ,  })
   .then(function(results){
-    res.json(JSON.parse(results));
     //console.log(results);
-    //return results;
-    //console.log('These results are awesome', results);
+    res.json(JSON.parse(results));
   })
   .catch(function(err){
     console.error('Oh no there was an error', err);
   });
 })
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now()
   }
-  // Continue to JSON Server router
   next()
 })
 
-// Use default router
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
