1ba3d2a7208c6c275f13531308ddf4717d3c76ce
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-25 11:51:28 +0900

    server edit.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const jsonServer = require('json-server')
+const jsonServer = require(‘json-server’)
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router(‘db.json’)
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
@@ -8,27 +8,27 @@ server.use(middlewares)
 // json-server 페이트서버 커스텀
 // google : 트랜드 결과 가져오기
 
-server.get('/google', (req, res) => {
-  //res.jsonp(req.query)
-  console.log(res.jsonp(req.query));
-  const val1 = '갤럭시노트8';
-  const val2 = '아이폰7플러스';
-
-  const googleTrends = require('google-trends-api');
+server.get(‘/google’, (req, res) => {
+  //console.log(' 결과 ‘, req.query.sc);
   
-  googleTrends.interestOverTime({keyword: [val1, val2], startTime: new Date('2017-08-01'), geo: "KR" ,  })
+ let search = req.query.sc.split(‘,’);
+  console.log(search);
+
+ const googleTrends = require(‘google-trends-api’);
+  
+ googleTrends.interestOverTime({keyword: search, startTime: new Date(‘2017-08-01’), geo: “KR” ,  })
   .then(function(results){
     //console.log(results);
     res.json(JSON.parse(results));
   })
   .catch(function(err){
-    console.error('Oh no there was an error', err);
+    console.error(‘Oh no there was an error’, err);
   });
 })
 
 server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
+  if (req.method === ‘POST’) {
     req.body.createdAt = Date.now()
   }
   next()
@@ -36,5 +36,5 @@ server.use((req, res, next) => {
 
 server.use(router)
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log(‘JSON Server is running’)
 })
