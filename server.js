c3d475ea65e628a805e1c645823de4bf2ab7bd49
downmix <downmix@downmix.net>
2017-08-25 11:55:10 +0900

    server.js 2 edit

--- server.js
+++ server.js
@@ -8,26 +8,15 @@ server.use(middlewares)
 // json-server 페이트서버 커스텀
 // google : 트랜드 결과 가져오기
 
-<<<<<<< HEAD
 server.get('/google', (req, res) => {
   //console.log(' 결과 ', req.query.sc);
   
   let search = req.query.sc.split(',');
-=======
-server.get(‘/google’, (req, res) => {
-  //console.log(' 결과 ‘, req.query.sc);
-  
- let search = req.query.sc.split(‘,’);
->>>>>>> 1ba3d2a7208c6c275f13531308ddf4717d3c76ce
   console.log(search);
 
  const googleTrends = require(‘google-trends-api’);
   
-<<<<<<< HEAD
   googleTrends.interestOverTime({keyword: search, startTime: new Date('2017-08-01'), geo: "KR" ,  })
-=======
- googleTrends.interestOverTime({keyword: search, startTime: new Date(‘2017-08-01’), geo: “KR” ,  })
->>>>>>> 1ba3d2a7208c6c275f13531308ddf4717d3c76ce
   .then(function(results){
     //console.log(results);
     res.json(JSON.parse(results));
