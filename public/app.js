27ed2908e5c1205c523301f7832b9e0d1c04f245
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-25 13:43:14 +0900

    Revised app.js file.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,27 @@
-var temp;
-const req = new XMLHttpRequest();
-let search = "신라면,진라면,너구리라면";
+let search = document.getElementById('input');
+let msg = document.getElementById('message');
+let btn = document.getElementById('inputBtn');
 
-req.open('GET', '/google?sc='+search, true);
-req.setRequestHeader('Content-type', 'application/json');
-req.send();
-req.onreadystatechange = function() {
-  if(req.readyState === XMLHttpRequest.DONE){
-    if(req.status == 200){
-      console.log(req.responseText);
-      temp = JSON.parse(req.responseText);
+btn.addEventListener('click', function () {
+  if(search.split(',').length <= 1){
+    msg.innerHTML = '두가지 이상을 입력해주세요';
+  } else {
+    getGoogleTd(search.split(','));
+  }
+});
+
+const getGoogleTd = (val) => {
+  var result;
+  const req = new XMLHttpRequest();
+  req.open('GET', '/google?sc='+val, true);
+  req.setRequestHeader('Content-type', 'application/json');
+  req.send();
+  req.onreadystatechange = function() {
+    if(req.readyState === XMLHttpRequest.DONE){
+      if(req.status == 200){
+        console.log(req.responseText);
+        result = JSON.parse(req.responseText);
+      }
     }
   }
 }
