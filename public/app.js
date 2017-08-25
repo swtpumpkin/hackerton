3c9936c213ad20a037f37cee284bda276b305d8e
JeongwonKim <swtpumpkin90@gmail.com>
2017-08-25 15:23:09 +0900

    Update app.js file. I added random add to array.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 const percent = 100;
 let average = [];
+let frontArrLeng = 0;
 let search = document.getElementById('input');
 let msg = document.getElementById('message');
 let btn = document.getElementById('inputBtn');
@@ -17,6 +18,9 @@ btn.addEventListener('click', function () {
   if(initValue.length <= 1){
     msg.innerHTML = '두가지 이상을 입력해주세요';
   } else {
+    // 초기 입력값의 배열의 길이를 설정하는 변수를 할당.
+    frontArrLeng = initValue.length;
+    console.log(frontArrLeng);
     //각 조건에 맞으면 다음 함수를 실행하는 함수를 작성.
     getGoogleTd(initValue);
   }
@@ -33,11 +37,26 @@ const getGoogleTd = (val) => {
         //console.log(req.responseText);
         average = JSON.parse(req.responseText);
         average = average.default.averages;
-        console.log(average);
-        divPercent(average);
+        //서버에서 받아온 배열의 길이를 탐색한다.
+        let getArrLeng = average.length;
+        //배열의 길이가 0보다 크면 서버에서 값을 받아왔기때문에 백분율을 구할 함수로 보낸다.
+        if(getArrLeng>0){
+          divPercent(average);
+        //배열의 길이가 0이면 배열의 값을 받아오지 못했으므로 랜덤값을 입력하는 함수로 보낸다.
+        }else if(getArrLeng === 0){
+          randomAdd(average);
+        }
       }
     }
   }
+}
+//빈 배열에 랜덤 숫자 담기.
+const randomAdd =  (average) => {
+  //입력된 배열의 길이만큼 랜덤 숫자를 담는다.
+  for(let num = 0; num < frontArrLeng; num++){
+    average.push((Math.random() * (90+1)) + 10)
+  }
+  divPercent(average);          
 }
 //나온 결과를 백분율로 배열에 새로 할당.
 const divPercent  = (average) => {
